ad9c94275cdcf7d6ac918e9dc270a2ad6b331368
uriarte, shane <sauuriarte@gmail.com>
2025-04-21 15:17:20 -0700

    initial testing done. testing suite completed. ready for experiment

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,10 @@ public class Main {
 
     public static void main(String[] args){
 
-       int[] test = {4, 6, 1, 2, 6, 8};
-       System.out.println(MedianOfMediansSelect.MedSelect(test, 3));
+        int[] test = {25, 18, 22, 20, 24};
+//        System.out.println("Merge select result: "+MergeSelect.MSelect(test, 2));
+//        System.out.println("Partition select result: "+PartitionSelect.PSelect(test, 2));
+        System.out.println("Median of Median select result: "+MedianOfMediansSelect.MedSelect(test, 2));
     }
 
     public static void TestSelect(){
@@ -22,6 +24,7 @@ public class Main {
 
         //generate array
         int[] arr = ArrayGenerator.generateArray(array_size);
+        int kth = 1; //placeholder
 
         //save array to file
         FileHandler.saveResults("data/input_arrays.txt", "Test: \n"+Arrays.toString(arr)+"\n");
@@ -32,12 +35,26 @@ public class Main {
             Timer timer = new Timer();
 
             //test merge select algorithm
-
+            timer.start();
+            int resultMerge = MergeSelect.MSelect(arr, kth);
+            timer.stop();
+            long timeMerge = timer.getElapsedTime();
+            FileHandler.saveResults("data/results.txt", "Test " + (i+1) + " Merge Select: " + timeMerge + "ms");
 
             //test partition select algorithm
+            timer.start();
+            int resultPartition = PartitionSelect.PSelect(arr, kth);
+            timer.stop();
+            long timePartition = timer.getElapsedTime();
+            FileHandler.saveResults("data/results.txt", "Test " + (i+1) + " Partition Select: " + timePartition + "ms");
 
 
             //test median of medians algorithm
+            timer.start();
+            int resultMedian = MedianOfMediansSelect.MedSelect(arr, kth);
+            timer.stop();
+            long timeMedian = timer.getElapsedTime();
+            FileHandler.saveResults("data/results.txt", "Test " + (i+1) + " Median Select: " + timeMedian + "ms");
 
 
             System.out.println("Test "+(i+1)+ " completed.");
